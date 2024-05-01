63fc38949ab75bc22356500ee07768b8ce96ced2
Jonas I. Liechti <j-i-l@t4d.ch>
2024-05-01 18:27:27 +0200

    moving wdws silently

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -98,7 +98,7 @@ in {
                     builtins.toString (x + 1 - (c * 10));
                 in [
                   "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                  "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+                  "$mod SHIFT, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
                 ]
               )
               10)
