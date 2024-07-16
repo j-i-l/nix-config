7b97694811621da1a3db7965813c74837713da6b
Jonas I. Liechti <j-i-l@t4d.ch>
2024-07-16 21:19:50 +0200

    qemu config for libvirt

--- hosts/nano/default.nix
+++ hosts/nano/default.nix
@@ -25,7 +25,22 @@
   };
 
   # enable virtualisation
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+
   programs.virt-manager.enable = true;
   
   virtualisation.lxd.enable = true;
